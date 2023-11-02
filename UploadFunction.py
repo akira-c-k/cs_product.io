7e15f250d5cdc9854671ce66a2a720522f054c0b
moriaki <cedar1122@gmail.com>
2023-11-03 01:17:26 +0900

    231103

--- UploadFunction.py
+++ UploadFunction.py
@@ -1,27 +1,32 @@
 import json
 import boto3
+from datetime import datetime
 
+# assign s3
 s3 = boto3.resource('s3')
 
-# assign bucket-name
-BUCKET_NAME = 'moriaki-cs-bucket-231030'
+print('*Loading lambda: parrot')
 
 def lambda_handler(event, context):
-    # get request-id
-    request_id = context.aws_request_id
-
-    # assign bucket-name and object-name
-    bucket = s3.Bucket(BUCKET_NAME)
-    object_key_name = f"{request_id}.json"
-
-    # generate object
-    obj = bucket.Object(object_key_name)
-
-    # upload the json data to the bucket
-    json_data = event
-    r = obj.put(Body = json.dumps(json_data))
+    # Read data
+    body = event["body"]
+    print('data:', body)
+    
+    # assign file
+    bucket = 'moriaki-cs-bucket-231030'
+    key = 'test_' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.txt'
+    file_contents = 'Lambda test'
+    
+    obj = s3.Object(bucket,key)
+    obj.put(Body=file_contents)
 
     return {
-        'request_id': request_id,
+        # 'isBase64Encoded': False,
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body':body
     }
